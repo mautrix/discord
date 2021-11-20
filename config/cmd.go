f97da27731dc486899c556b0a1576de881b13f52
Gary Kramlich <grim@reaperworld.com>
2021-11-20 03:33:19 -0600

    Add a force option to the generate-config command

--- config/cmd.go
+++ config/cmd.go
@@ -1,15 +1,26 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
 	"gitlab.com/beeper/discord/globals"
 )
 
 type Cmd struct {
 	HomeserverAddress string `kong:"arg,help='The url to for the homeserver',required='1'"`
 	Domain            string `kong:"arg,help='The domain for the homeserver',required='1'"`
+
+	Force bool `kong:"flag,help='Overwrite an existing configuration file if one already exists',short='f',default='0'"`
 }
 
 func (c *Cmd) Run(g *globals.Globals) error {
+	if _, err := os.Stat(g.Config); err == nil {
+		if c.Force == false {
+			return fmt.Errorf("file %q exists, use -f to overwrite", g.Config)
+		}
+	}
+
 	cfg := &Config{
 		Homeserver: homeserver{
 			Address: c.HomeserverAddress,

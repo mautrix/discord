d0fb07587458f0097ab3eb1a6613cdc1ef3ab0ca
Tulir Asokan <tulir@maunium.net>
2023-01-16 17:24:04 +0200

    Add db migration to fix broken double puppets

--- database/upgrades/00-latest-revision.sql
+++ database/upgrades/00-latest-revision.sql
@@ -1,4 +1,4 @@
--- v0 -> v9: Latest revision
+-- v0 -> v10: Latest revision
 
 CREATE TABLE guild (
     dcid       TEXT PRIMARY KEY,
---
+++ database/upgrades/10-remove-broken-double-puppets.sql
@@ -0,0 +1,2 @@
+-- v10: Remove double puppet ghosts added while there was a bug in the bridge
+DELETE FROM puppet WHERE id='';

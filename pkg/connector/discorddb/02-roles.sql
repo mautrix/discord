fa25e2784a824a2f18e8cea074331d1d5bc6b630
Skip R <tinyslices@gmail.com>
2026-02-25 06:28:28 -0800

    db: init roles

--- pkg/connector/discorddb/02-roles.sql
+++ pkg/connector/discorddb/02-roles.sql
@@ -1,16 +1,4 @@
--- v0 -> v2 (compatible with v1+): latest schema
-
--- https://docs.discord.com/developers/resources/emoji#emoji-object
-CREATE TABLE custom_emoji (
-    discord_id TEXT NOT NULL,
-    name       TEXT NOT NULL,
-    animated   BOOLEAN NOT NULL,
-
-    mxc        TEXT,
-
-    PRIMARY KEY (discord_id)
-);
-CREATE INDEX custom_emoji_mxc_idx ON custom_emoji (mxc);
+-- v1 -> v2 (compatible with v1+): roles
 
 CREATE TABLE role (
     discord_guild_id TEXT NOT NULL,

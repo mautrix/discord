6a2d8d569e353239faf4a29e4c7a96a837664a0f
Skip R <tinyslices@gmail.com>
2026-02-25 07:29:39 -0800

    db: add thread migration

--- pkg/connector/discorddb/03-threads.sql
+++ pkg/connector/discorddb/03-threads.sql
@@ -1,34 +1,4 @@
--- v0 -> v3 (compatible with v1+): latest schema
-
--- https://docs.discord.com/developers/resources/emoji#emoji-object
-CREATE TABLE custom_emoji (
-    discord_id TEXT NOT NULL,
-    name       TEXT NOT NULL,
-    animated   BOOLEAN NOT NULL,
-
-    mxc        TEXT,
-
-    PRIMARY KEY (discord_id)
-);
-CREATE INDEX custom_emoji_mxc_idx ON custom_emoji (mxc);
-
-CREATE TABLE role (
-    discord_guild_id TEXT NOT NULL,
-    discord_id       TEXT NOT NULL,
-
-    name             TEXT NOT NULL,
-    icon             TEXT,
-
-    mentionable      BOOLEAN NOT NULL,
-    managed          BOOLEAN NOT NULL,
-    hoist            BOOLEAN NOT NULL,
-
-    color            INTEGER NOT NULL,
-    position         INTEGER NOT NULL,
-    permissions      BIGINT  NOT NULL,
-
-    PRIMARY KEY (discord_guild_id, discord_id)
-);
+-- v2 -> v3 (compatible with v1+): threads
 
 CREATE TABLE discord_thread (
     -- The ID of the UserLogin that witnessed the thread.

e8a6e93ce0eacb21c4a5d5504bf2bc62643d4d55
Gerardo Rodriguez <gc0814@gmail.com>
2026-06-16 22:44:58 -0400

    msgconv: correctly send `OriginalContentType` on outgoing attachments (#232)
    Co-authored-by: Skip R <tinyslices@gmail.com>

--- pkg/msgconv/from-matrix.go
+++ pkg/msgconv/from-matrix.go
@@ -55,7 +55,7 @@ func parseAllowedLinkPreviews(raw map[string]any) []string {
 	return allowedLinkPreviews
 }
 
-func uploadDiscordAttachment(cli *http.Client, url string, data []byte, contentType string) error {
+func uploadDiscordAttachment(cli *http.Client, url string, data []byte) error {
 	req, err := http.NewRequest(http.MethodPut, url, bytes.NewReader(data))
 	if err != nil {
 		return err
@@ -64,10 +64,9 @@ func uploadDiscordAttachment(cli *http.Client, url string, data []byte, contentT
 	for key, value := range discordgo.DroidBaseHeaders {
 		req.Header.Set(key, value)
 	}
-	if contentType == "" {
-		contentType = "application/octet-stream"
-	}
-	req.Header.Set("Content-Type", contentType)
+	// The first-party web client does not set a content type, so we shouldn't
+	// either.
+	req.Header.Set("Content-Type", "application/octet-stream")
 	req.Header.Set("Referer", "https://discord.com/")
 	req.Header.Set("Sec-Fetch-Dest", "empty")
 	req.Header.Set("Sec-Fetch-Mode", "cors")
@@ -161,6 +160,9 @@ func (mc *MessageConverter) ToDiscord(
 			ID:       "0",
 			Filename: filename,
 		}
+		if content.Info != nil {
+			att.OriginalContentType = content.Info.MimeType
+		}
 		if voiceMeta != nil {
 			flags := int(discordgo.MessageFlagsIsVoiceMessage)
 			req.Flags = &flags
@@ -172,9 +174,10 @@ func (mc *MessageConverter) ToDiscord(
 		uploadID := mc.NextDiscordUploadID()
 		log.Debug().Str("upload_id", uploadID).Msg("Preparing attachment")
 		filePrep := &discordgo.FilePrepare{
-			Size: len(mediaData),
-			Name: att.Filename,
-			ID:   uploadID,
+			Size:                len(mediaData),
+			Name:                att.Filename,
+			ID:                  uploadID,
+			OriginalContentType: att.OriginalContentType,
 		}
 		prep, err := session.ChannelAttachmentCreate(channelID, &discordgo.ReqPrepareAttachments{
 			Files: []*discordgo.FilePrepare{filePrep},
@@ -188,7 +191,7 @@ func (mc *MessageConverter) ToDiscord(
 		prepared := prep.Attachments[0]
 		att.UploadedFilename = prepared.UploadFilename
 
-		err = uploadDiscordAttachment(session.Client, prepared.UploadURL, mediaData, att.OriginalContentType)
+		err = uploadDiscordAttachment(session.Client, prepared.UploadURL, mediaData)
 		if err != nil {
 			log.Err(err).Msg("Failed to reupload Discord attachment after preparing")
 			return nil, bridgev2.ErrMediaReuploadFailed
